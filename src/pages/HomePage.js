082e8ea14bbba0ec61025fba2b6889cdcb8c2e4e
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-08-12 23:47:49 -0500

    =added home page, search page & show page components

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <section className="home-page">
+            <div className="container">
+                <h1>Home Page</h1>
+            </div>
+            
+        </section>
+    )
+}
+
+export default HomePage
---
+++ src/pages/ShowPages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowPages = () => {
+    return (
+        <div>
+            <h1>Show Details</h1>
+        </div>
+    )
+}
+
+export default ShowPages
