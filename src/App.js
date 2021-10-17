9a721cc373a5ed9562387d1700a9b28733f4cecd
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-10-17 00:02:09 -0500

    updated components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
-<<<<<<< HEAD
+import Footer from "./components/layout/Footer";
 
-=======
-import Footer from './components/layout/Footer'
- 
->>>>>>> 2ab5e6a9f271fd7f2b048959907a79137239928a
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import ShowPages from "./pages/ShowPage";
 
 function App() {
-<<<<<<< HEAD
 	return (
 		<BrowserRouter>
 			<Header />
@@ -22,24 +17,9 @@ function App() {
 				<Route path="/search" component={SearchPage} />
 				<Route path="/shows/:id" component={ShowPages} />
 			</Switch>
+			<Footer />
 		</BrowserRouter>
 	);
-=======
-  return (
-    <BrowserRouter>
-
-      <Header />
-
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/search" component={SearchPage} />
-        <Route path="/shows/:id" component={ShowPages} />
-        
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
->>>>>>> 2ab5e6a9f271fd7f2b048959907a79137239928a
 }
 
 export default App;
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,22 +1,6 @@
 import React from "react";
 
 const Footer = () => {
-<<<<<<< HEAD
-	return (
-		<section className="footer">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12">
-						<p>
-							&copy; Copyright 2021. All rights reserved. Development by
-							<a href="http://mahmoudosman.com">Mahmoud Osman</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
-=======
   return <section className="footer">
     <div className="container">
       <div className="row">
@@ -28,7 +12,6 @@ const Footer = () => {
       </div>
     </div>
   </section>;
->>>>>>> 2ab5e6a9f271fd7f2b048959907a79137239928a
 };
 
 export default Footer;
